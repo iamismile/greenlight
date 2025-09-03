722d99c6c343aa2b90be6cc6d73a9536aa397ab7
Ismile Hossain <iamismile.dev@gmail.com>
2025-09-03 11:33:57 +0600

    caught SIGINT & SIGTERM signals

---
+++ cmd/api/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	// Server setup
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		ErrorLog:     log.New(app.logger, "", 0),
+	}
+
+	// Start a background go routine
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+
+		// Exist the app with a 0 (success) status code
+		os.Exit(0)
+	}()
+
+	// Start the HTTP server
+	app.logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.config.env,
+	})
+
+	return srv.ListenAndServe()
+}
